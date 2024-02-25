42ecde5c5e2eb1482ba59d850fabdb89b13da401
Current <reit120807@gmail.com>
2024-02-26 02:21:04 +0700

    Commit changes

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-import './css/style.css'
+const houseList = [
+    document.querySelector(".house-selector__button-1"),
+    document.querySelector(".house-selector__button-2"),
+    document.querySelector(".house-selector__button-3"),
+    document.querySelector(".house-selector__button-4"),
+    document.querySelector(".house-selector__button-5")
+]
+
+let currentHouse = 0;
+
+for (let i = 0; i < houseList.length; i++) {
+    houseList[i].addEventListener("click", () => {
+        if (i !== currentHouse) {
+            houseList[i].classList.add('house-selected');
+            houseList[currentHouse].classList.remove('house-selected');
+            currentHouse = i;
+        }
+    });
+}
